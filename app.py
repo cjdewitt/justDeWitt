2e3186f432c2f2c434252f5ca1ae65047a7a1d42
Cory DeWitt <cjdewitt@usc.edu>
2024-11-17 00:03:22 -0800

    Fix binding to port and use Gunicorn

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def chat():
         return jsonify({'reply': 'Sorry, an error occurred while processing your request.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
